6dd92b500b2f6e2abf646e13383f9a0b492671b2
kulendra <kulendra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-23 20:29:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-extplorer/trunk@266519 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ helpers/iframe.php
@@ -0,0 +1,2 @@
+<p>Find wp-eXtplorer useful? <a href='http://kulendra.net/Wordpress-Extensions/wp-extplorer.html'>Help us give you more, consider purchasing wp-eXtplorer for $5.50.</a></p>
+<iframe src='<?php echo get_option('home').DS.'wp-content'.DS.'plugins'.DS.'wp-eXtplorer'.DS.'helpers'.DS;?>main.php' height="500px" width="900px"></iframe>
